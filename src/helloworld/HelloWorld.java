d99545dd3c8626587c3afe6f66b8f4254c82bbd3
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame1-THINK>
2014-11-20 11:04:53 +0200

    before lunc version

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,12 +5,14 @@
  */
 package helloworld;
 
+// katso, että luokat on javafx (fix imports voi valita jonkun muunkin)
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -22,23 +24,52 @@ public class HelloWorld extends Application {
     @Override
     // stage on se sinisellä oleva nimi ja nappulat
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        // root on ns. mainlayout joka saisi sisältää vain muita layoutteja
+        VBox root = new VBox();
         
+        // text voi sisältää vain tekstiä, label sekä kuvan/ikonin että tekstin
+        // label vie enemmän muistia
+        
+        Label nameLabel = new Label("Nimi");
+        TextField nameField = new TextField();
+        
+        Label addressLabel = new Label("Osoite");
+        TextField addressField = new TextField();
+        
+        Label phoneLabel = new Label("Puhelinnumero");
+        TextField phoneField = new TextField();
+        
+        // Do layout for textfield components
+        VBox textFieldLayout = new VBox();
+        textFieldLayout.setStyle("-fx-padding:10;-fx-spacing:10");
+        textFieldLayout.getChildren().add(nameLabel);
+        textFieldLayout.getChildren().add(nameField);
+        textFieldLayout.getChildren().add(addressLabel);
+        textFieldLayout.getChildren().add(addressField);
+        textFieldLayout.getChildren().add(phoneLabel);
+        textFieldLayout.getChildren().add(phoneField);
+        
+        Button closeButton = new Button("Sulje");
+        closeButton.setStyle("-fx-color:red");
+        
+        Button saveButton = new Button("Tallenna");     
+        saveButton.setStyle("-fx-color:blue");
+        
+        // Do layout for button components
+        HBox buttonLayout = new HBox();
+        // spacing pistää buttonien väliin väin, padding niiden ympärille
+        buttonLayout.setStyle("-fx-spacing:10;-fx-padding:10");
+        buttonLayout.getChildren().add(closeButton);
+        buttonLayout.getChildren().add(saveButton);
+        
+        root.getChildren().add(textFieldLayout);
+        root.getChildren().add(buttonLayout);
+                
         // scene on se valkoinen alue
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Idea");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
